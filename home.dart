d125b901df55c396f0cdee3ab43476c455136cb3
milonzzz <milandamoowali@gmail.com>
2024-05-16 13:23:52 +0800

    Add files via upload

---
+++ home.dart
@@ -0,0 +1,13 @@
+void main(){
+  int beratbadan = 26;
+
+if (beratbadan >= 46 ){
+  print("Hello Word");
+  } else { 
+    print("Berat Badan Anda"); 
+  }
+  /*
+  *ini hanyalah sebuah test
+  *
+  */
+  } 
